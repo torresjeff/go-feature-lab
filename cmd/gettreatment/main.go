3a13b19ff20b9c48c744994fec593f3546504acf
Jeff <torres.jeff@hotmail.com>
2023-05-01 23:40:49 +0200

    Fix error type

--- cmd/gettreatment/main.go
+++ cmd/gettreatment/main.go
@@ -27,7 +27,7 @@ func main() {
 	featureName := "ChangeBuyButtonColor"
 
 	for _, userId := range userIds {
-		treatment, err := featureLab.GetTreatment("FeatureLab", featureName, userId)
+		treatment, err := featureLab.GetTreatment("FeatureLabs", featureName, userId)
 		if err != nil {
 			log.Printf(err.Error())
 		} else {
--- featurelab/error.go
+++ featurelab/error.go
@@ -4,21 +4,21 @@ type ErrorType uint32
 
 const (
 	// Error codes returned by API, these are different from the HTTP status codes
-	ErrBadRequest          uint32 = 400
-	ErrNotFound            uint32 = 404
-	ErrInternalServerError uint32 = 500
-	ErrInvalidTreatment    uint32 = 5000
+	ErrBadRequest          ErrorType = 400
+	ErrNotFound            ErrorType = 404
+	ErrInternalServerError ErrorType = 500
+	ErrInvalidTreatment    ErrorType = 5000
 )
 
 type Error struct {
-	Code    uint32 `json:"code"`
-	Message string `json:"message"`
+	Code    ErrorType `json:"code"`
+	Message string    `json:"message"`
 }
 
 func (e *Error) Error() string {
 	return e.Message
 }
 
-func NewError(code uint32, message string) *Error {
+func NewError(code ErrorType, message string) *Error {
 	return &Error{code, message}
 }
